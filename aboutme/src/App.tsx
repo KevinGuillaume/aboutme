0a23cc638472826022727aaa9c13ff72be9838d4
KevinGuillaume <keving3492@gmail.com>
2025-10-15 23:02:19 -0500

    tailwind and potential backgrounds

--- aboutme/src/App.tsx
+++ aboutme/src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="relative font-dm min-h-screen">
+  {/* Background pic */}
+  <div className="fixed inset-0 z-1 bg-howl bg-cover bg-no-repeat bg-center"></div>
+
+  {/* Page content */}
+  <div className="relative z-10 flex flex-col">
+    {/* top part */}
+    <main className="flex-1 flex flex-col p-4">
+      <h2 className="text-[6rem] md:text-[8rem] font-medium leading-none text-gray-100">
+        Kevin Guillaume
+      </h2>
+      <p className="mt-6 text-xl text-gray-300 max-w-2xl">
+      I’m a software engineer passionate about exploring the DeFi space 
+      and its real-world applications. I focus on building high-performance applications that users enjoy,
+       blending practical innovation with seamless user experiences.
       </p>
-    </>
+    </main>
+
+    {/* sections */}
+    <section id="about" className="px-10 py-20">
+      <h3 className="text-3xl font-semibold mb-4">About Me</h3>
+      <p className="text-lg leading-relaxed text-gray-600 max-w-3xl">
+      </p>
+    </section>
+
+    <section id="experience" className="px-10 py-20">
+      <h3 className="text-3xl font-semibold mb-4">Experience</h3>
+      <p className="text-lg text-gray-600 max-w-3xl">
+      </p>
+    </section>
+
+    <section id="contact" className="px-10 py-20">
+      <h3 className="text-3xl font-semibold mb-4">Contact</h3>
+      <p className="text-lg text-gray-600 max-w-3xl">
+      </p>
+    </section>
+  </div>
+</div>
+
   )
 }
 
---
+++ aboutme/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      backgroundImage: {
+        howl: "url('/src/assets/howls2.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
+
